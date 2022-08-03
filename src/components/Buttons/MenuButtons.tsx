d7bc72e46d7e9fc2963b35cc2a1b0caaee7c7133
Rizel Scarlett <blackgirlbytes@github.com>
2022-08-02 22:34:32 -0400

    update pdfs

--- src/components/Buttons/MenuButtons.tsx
+++ src/components/Buttons/MenuButtons.tsx
@@ -5,8 +5,8 @@ const MenuButtons = () => {
     return (
         <section className="pt-12">
             <MenuButton url="/blackgyalbites-nextjs/menu-pdf-food.pdf" menu_type="Food" />
-            <MenuButton url="/blackgyalbites-nextjs/menu-pdf-food.pdf" menu_type="Drinks" />
-            <MenuButton url="/blackgyalbites-nextjs/menu-pdf-food.pdf" menu_type="Catering" />
+            <MenuButton url="/blackgyalbites-nextjs/menu-pdf-drinks.pdf" menu_type="Drinks" />
+            <MenuButton url="/blackgyalbites-nextjs/menu-pdf-catering.pdf" menu_type="Catering" />
         </section>
 
     )
